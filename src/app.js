06c85a5dd9e3e6c7b5886ad714228660c12e00f3
drk <drk@gmail.com>
2022-02-04 10:12:06 -0300

    config morgan

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const morgan = require('./config/morgan')
+const config = require('./config/config')
+const cors = require('cors')
+const compression = require('compression')
+const app = express()
+
+//DEVELOPMENT
+const isProduction = process.env.NODE_ENV === 'production'
+
+//SETUP BODY PARSER
+app.use(express.urlencoded({ extended: true, limit: 1.5 * 1024 * 1024 }));
+app.use(express.json({ limit: 1.5 * 1024 * 1024 }));
+
+//CONFIG
+if(config.env !== 'test'){
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler)
+}
+
+if(config.env !== 'production') app.use(cors())
+app.disable('x-powered-by')
+app.use(compression())
+
+module.exports = app
--- src/server.js
+++ src/server.js
@@ -1,22 +1,8 @@
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const compression = require('compression')
-const app = express()
 const config = require('./config/config')
+const app = require('./app')
 
-//DEVELOPMENT
-const isProduction = process.env.NODE_ENV === 'production'
 
-//SETUP BODY PARSER
-app.use(express.urlencoded({ extended: true, limit: 1.5 * 1024 * 1024 }));
-app.use(express.json({ limit: 1.5 * 1024 * 1024 }));
-
-//CONFIG
-if (!isProduction) app.use(morgan('dev'))
-if (!isProduction) app.use(cors())
-app.disable('x-powered-by')
-app.use(compression())
 
 //LISTEN
 app.listen(config.port, () => {
