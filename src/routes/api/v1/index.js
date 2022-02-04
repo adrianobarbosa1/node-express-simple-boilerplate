ab21e7f2bb835202c13b37a5408deb482ff2d725
drk <drk@gmail.com>
2022-02-04 10:50:38 -0300

    prettie code

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router();
 
-router.use('/ideas', require('./ideas.route'))
-// router.use('/minions', require('./minions.route'))
-// router.use('/meetings', require('./meeting.route'))
+router.use('/user', require('./user.route'))
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const config = require('./config/config')
 const app = require('./app')
 
-
-
 //LISTEN
 app.listen(config.port, () => {
   console.log(`Rodando na porta ${config.port}`)
