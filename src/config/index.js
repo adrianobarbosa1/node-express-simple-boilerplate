9ebcd607806544fe17edbfb53f87dd7a0eb5d25f
drk <drk@gmail.com>
2022-02-17 17:27:32 -0300

    add logger

--- src/config/index.js
+++
@@ -1,7 +0,0 @@
-const PORT = require('../server')
-
-module.exports = {
-  // secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : "WREFFEFD8FEWF231FEW64654FEWf354doihfeonkf564F",
-  api: process.env.NODE_ENV === 'production' ? "https://api.anapolis.go.gov.br/apirh" : `http://localhost:${PORT}`,
- // loja: process.env.NODE_ENV === 'production' ? "firebase" : 'http://localhost:8000',
-}
